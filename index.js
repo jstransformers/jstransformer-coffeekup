9a02d4696d622909010da5c8ba480f7218b92e49
Rob Loach <robloach@gmail.com>
2019-01-14 13:16:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ exports.outputFormat = 'html'
 
 exports.compile = function (str, options) {
   options = options || {}
+
   return coffeekup.compile(str, options)
 }
+
 exports.render = function (str, options, locals) {
   return coffeekup.render(str, locals, options)
 }
